2c91fbe2f93b0ce8d962e7b7346958717c747f32
maxim <xydenswork@gmail.com>
2020-11-27 15:34:35 +0500

    настроить eslint

--- pages/_app.js
+++ pages/_app.js
@@ -4,34 +4,38 @@ import Head from 'next/head';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-const theme = createMuiTheme()
+const theme = createMuiTheme();
 
 export default function MyApp(props) {
-    const { Component, pageProps } = props;
+  const { Component, pageProps } = props;
 
-    React.useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-            jssStyles.parentElement.removeChild(jssStyles);
-        }
-    }, []);
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-    return (
-        <React.Fragment>
-            <Head>
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-            </Head>
-            <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </React.Fragment>
-    );
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 MyApp.propTypes = {
-    Component: PropTypes.elementType.isRequired,
-    pageProps: PropTypes.object.isRequired,
+  Component: PropTypes.elementType.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  pageProps: PropTypes.object.isRequired
 };
--- pages/clean.js
+++ pages/clean.js
@@ -1,130 +1,137 @@
-import React, {useReducer, useState, useEffect, useRef} from 'react'
-import CleanForm from "../components/CleanForm";
-import {CleanerService} from "../lib/cleaner/cleaner-service";
-import throttle from "../utils/throttle";
+import React, { useReducer, useState, useEffect } from 'react';
 import Head from 'next/head';
+import CleanForm from '../components/CleanForm';
+import { CleanerService } from '../lib/cleaner/cleaner-service';
+import throttle from '../utils/throttle';
 
 const reducer = (state, action) => {
-    console.log({action, state})
-    switch (action.type) {
-        case 'LOAD_DATA':
-            return {
-                ...state,
-                pending: true,data: null, error: null
-            }
-        case 'LOAD_DATA_SUCCESS':
-            return {
-                ...state,
-                data: action.payload,
-                error: null,
-                pending: false
-            }
-        case 'LOAD_DATA_FAILURE':
-            return {
-                ...state,
-                error: action.payload,
-                data: null,
-                pending: false
-            }
-        default:
-            throw new Error()
-    }
-}
+  console.log({ action, state });
+  switch (action.type) {
+    case 'LOAD_DATA':
+      return {
+        ...state,
+        pending: true,
+        data: null,
+        error: null
+      };
+    case 'LOAD_DATA_SUCCESS':
+      return {
+        ...state,
+        data: action.payload,
+        error: null,
+        pending: false
+      };
+    case 'LOAD_DATA_FAILURE':
+      return {
+        ...state,
+        error: action.payload,
+        data: null,
+        pending: false
+      };
+    default:
+      throw new Error();
+  }
+};
 
 const defaultState = {
-    pending: false,
-    data: null,
-    error: null,
-}
+  pending: false,
+  data: null,
+  error: null
+};
 
 const handleResponseError = async res => {
-    if(res.status !== 200) {
-        let json
-        try {
-            json = await res.json()
-        } catch (e) {
-        }
-        if(json && json.error) {
-            throw new Error(json.error.message)
-        } else {
-            throw new Error('Server error')
-        }
-    } else {
-        return res
+  if (res.status !== 200) {
+    let json;
+    try {
+      json = await res.json();
+    } catch (e) {
+      console.error(e);
     }
-}
+    if (json && json.error) {
+      throw new Error(json.error.message);
+    } else {
+      throw new Error('Server error');
+    }
+  } else {
+    return res;
+  }
+};
 
-const getData = (query) =>
-    fetch('/api/clean?query=' + encodeURIComponent(query))
-        .then(handleResponseError)
-        .then(r => r.json())
+const getData = query =>
+  fetch(`/api/clean?query=${encodeURIComponent(query)}`)
+    .then(handleResponseError)
+    .then(r => r.json());
 
 const callback = throttle(async (query, dispatch) => {
-        dispatch({
-            type: 'LOAD_DATA'
-        })
-        try {
-            const data = await getData(query)
-            dispatch({
-                type: 'LOAD_DATA_SUCCESS',
-                payload: data,
-            })
-        } catch (e) {
-            console.error('request error', e)
-            dispatch({
-                type: 'LOAD_DATA_FAILURE',
-                payload: e,
-            })
-        }
+  dispatch({
+    type: 'LOAD_DATA'
+  });
+  try {
+    const data = await getData(query);
+    dispatch({
+      type: 'LOAD_DATA_SUCCESS',
+      payload: data
+    });
+  } catch (e) {
+    console.error('request error', e);
+    dispatch({
+      type: 'LOAD_DATA_FAILURE',
+      payload: e
+    });
+  }
 }, 500);
 
 export default function Clean(props) {
-    console.log({props})
+  console.log({ props });
 
-    const [state, dispatch] = useReducer(reducer, {
-        ...defaultState,
-        data: props.initialRequest  ? props.initialRequest.data : null,
-    })
-    const [query, setQuery] = useState(props.initialRequest && props.initialRequest.query || '')
+  const [state, dispatch] = useReducer(reducer, {
+    ...defaultState,
+    data: props.initialRequest ? props.initialRequest.data : null
+  });
+  const [query, setQuery] = useState(
+    (props.initialRequest && props.initialRequest.query) || ''
+  );
 
-    const queryHandler = React.useCallback(callback, [])
+  const queryHandler = React.useCallback(callback, []);
 
-    useEffect(() => {
-        if(!state.data || !Array.isArray(state.data)|| !state.data.find(item => item.source === query)) {
-            queryHandler(query, dispatch)
-        }
-    }, [query])
+  useEffect(() => {
+    if (
+      !state.data ||
+      !Array.isArray(state.data) ||
+      !state.data.find(item => item.source === query)
+    ) {
+      queryHandler(query, dispatch);
+    }
+  }, [query]);
 
-       return <>
-           <Head>
-               <title>Cleaner</title>
-           </Head>
-           <CleanForm
-       data={state.data}
-       pending={state.pending}
-       query={query}
-       error={state.error}
-       onQueryChange={setQuery}
-       />
-       </>
-
+  return (
+    <>
+      <Head>
+        <title>Cleaner</title>
+      </Head>
+      <CleanForm
+        data={state.data}
+        pending={state.pending}
+        query={query}
+        error={state.error}
+        onQueryChange={setQuery}
+      />
+    </>
+  );
 }
 
-const cleaner = new CleanerService()
-
 export async function getServerSideProps() {
-    try {
-        const initialRequest = await cleaner.performRandomQuery()
-        return {
-            props: {
-                initialRequest
-            }
-        }
-    } catch (e) {
-        console.error(e)
-        return {
-            props: {}
-        }
-    }
-
+  try {
+    const initialRequest = await CleanerService.performRandomQuery();
+    return {
+      props: {
+        initialRequest
+      }
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: {}
+    };
+  }
 }
